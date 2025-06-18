a339d6defb16feebd8f56e3848e264e7e1a716c5
Yainish <ginesvidaldiaz@gmail.com>
2025-06-18 02:33:23 +0200

    Reestructuración

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function blast() {
+    document.getElementById("ele").style.display = "none";
+    document.getElementById("bla").style.display = "block";
+}
+
+function electric() {
+    document.getElementById("ele").style.display = "block";
+    document.getElementById("bla").style.display = "none";
+}
